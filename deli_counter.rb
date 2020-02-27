6cd4ad792b8c5043a0251b050a3726904a927f5d
Hannah Kofkin <hkofkin@gmail.com>
2020-02-27 21:52:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,9 +13,11 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << "#{name}"
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+$counter = 0
+def take_a_number(katz_deli)
+#  katz_deli << "#{name}"
+  katz_deli << $counter += 1
+  puts "Welcome. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
